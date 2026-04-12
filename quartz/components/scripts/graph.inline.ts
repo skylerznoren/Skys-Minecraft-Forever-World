1610e722f4b6708d15f613e490c848b54ae06bb5
Skyler Noren <sznoren9@gmail.com>
2026-04-12 07:09:49 -0700

    Quartz sync: Apr 12, 2026, 7:09 AM

--- quartz/components/scripts/graph.inline.ts
+++ quartz/components/scripts/graph.inline.ts
@@ -449,9 +449,9 @@ async function renderGraph(graph: HTMLElement, fullSlug: FullSlug) {
     linkRenderData.push(linkRenderDatum)
   }
 
-  let currentTransform = zoomIdentity.translate(-width / 2, -height / 2).scale(2)
-  stage.scale.set(2, 2)
-  stage.position.set(-width / 2, -height / 2)
+  let currentTransform = zoomIdentity.translate(-width / 2, -height / 2).scale(1.6)
+stage.scale.set(1.6, 1.6)
+stage.position.set(-width / 2, -height / 2)
 
   if (enableDrag) {
     select<HTMLCanvasElement, NodeData | undefined>(app.canvas).call(
