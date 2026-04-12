d1ff53bd093213d9fadecefa2f8427b69bc19e60
Skyler Noren <sznoren9@gmail.com>
2026-04-12 06:03:02 -0700

    Quartz sync: Apr 12, 2026, 6:03 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -41,7 +41,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph({ localGraph: { height: 750, showTags: false, repelForce: 1.5, linkDistance: 60, scale: 5, fontSize: 1.2 }, globalGraph: { showTags: false } }),
+    Component.Graph({ localGraph: { height: 750, showTags: false, repelForce: 1.5, linkDistance: 60, scale: 1.5, fontSize: 3 }, globalGraph: { showTags: false } }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
